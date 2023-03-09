b2d374beb768262061985f3a4e93e653e56d0459
doki <shldhee@yahoo.co.kr>
2023-03-09 15:21:15 +0900

    studying p188

--- project/server/src/resolvers/User.ts
+++ project/server/src/resolvers/User.ts
@@ -12,6 +12,32 @@ export class SignUpInput {
   @Field() @IsString() password: string;
 }
 
+@InputType({ description: '로그인 인풋 데이터' })
+export class LoginInput {
+  @Field() @IsString() emailOrUsername: string;
+
+  @Field() @IsString() password: string;
+}
+
+@InputType({ description: '필드 에러 타입' })
+class FieldError {
+  @Field() field: string;
+
+  @Field() message: string;
+}
+
+@InputType({ description: '로그인 반환 데이터' })
+class LoginResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+
+  @Field({ nullable: true })
+  accessToken?: string;
+}
+
 @Resolver(User)
 export class UserResolver {
   @Mutation(() => User)
@@ -27,5 +53,26 @@ export class UserResolver {
 
     await User.insert(newUser);
     return newUser;
+  }
+
+  @Mutation(() => LoginResponse)
+  public async login(@Arg('loginInput') loginInput: LoginInput): Promise<LoginRespone> {
+    const { email, password } = loginInput;
+
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return {
+        errors: [{ field: 'email', message: '해당하는 유저가 없습니다.' }],
+      };
+    }
+
+    const isValid = await argon2.verify(user.password, password);
+    if (!isValid) {
+      return {
+        errors: [{ field: 'password', message: '비밀번호를 올바르게 입력해주세요.' }],
+      };
+    }
+
+    return user;
   }
 }
